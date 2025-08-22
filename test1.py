1017ae53f4d991b8de0a2c59ed00532c3d497814
KarthikPasam Premkumar <karthikpasam.premkumar@recodesolutions.com>
2025-08-22 15:28:35 +0530

    update_test

--- test1.py
+++ test1.py
@@ -1,16 +1,1 @@
-'''for i in range(2,101):
-    for j in range(2,i):
-        if i%j==0:
-            break
-    else:
-        print(i)
-
-
-def fibo(n):
-    if n<=1:
-        return n
-    else:
-        return fibo(n-1) + fibo(n-2)
-for i in range(21):
-    print(fibo(i))'''
-
+#test chnage
